e949dfcb448921b6c29a4dbe53b958191e7c67eb
ahmed-muhamad <ahmed.elzowawi@outlook.com>
2023-03-17 23:38:30 +0100

    Feature: user being able to enter the size of the triangle

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,10 +1,24 @@
-let star = '*';
-let space = ' ';
-for (let row = 0; row < 10; row++) {
-	for (let col = 10 ; col > row; col--) {
-		space += ' ';
-	}
-	console.log(space + star);
-	star += "*";
-	space = ' ';
-}
+const readline = require("readline").createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+const printTriangle = () => {
+	readline.question("Enter a number: ", (userInputNumber) => {
+		console.log('\n');
+		let star = '*';
+		let space = ' ';
+		for (let row = 0; row < userInputNumber; row++) {
+			for (let col = userInputNumber ; col > row; col--) {
+				space += ' ';
+			}
+			console.log(space + star);
+			star += "*";
+			space = ' ';
+		}
+	console.log('\n');
+	printTriangle();
+	});
+};
+
+printTriangle();
